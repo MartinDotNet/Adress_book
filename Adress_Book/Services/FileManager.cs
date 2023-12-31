c21985f85c709560e07c1b93c52ef2ebf33036db
MartinDotNet <143500729+MartinDotNet@users.noreply.github.com>
2023-12-31 19:23:17 +0100

    Add files via upload

---
+++ Adress_Book/Services/FileManager.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Adress_Book.Services;
+
+
+public interface IFileManager 
+{
+    
+    
+    bool SaveAdress(string content);
+
+    string GetAdress();
+}
+
+
+public class FileManager(string filePath) : IFileManager
+{
+        private readonly string _filePath = filePath;
+
+    public string GetAdress()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SaveAdress(string content)
+    {
+        throw new NotImplementedException();
+    }
+}
