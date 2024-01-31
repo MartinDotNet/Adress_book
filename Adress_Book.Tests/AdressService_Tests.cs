27cac0977ecf47062fdf5df23dbc5a6200089b67
MartinDotNet <143500729+MartinDotNet@users.noreply.github.com>
2024-01-31 23:09:19 +0100

    Add files via upload

---
+++ Adress_Book.Tests/AdressService_Tests.cs
@@ -0,0 +1,33 @@
+﻿
+using Adress_Book.Models;
+using Adress_Book.Services;
+
+namespace Adress_Book.Tests;
+
+public class AdressService_Tests
+{
+    [Fact]
+    public void AddPerson_Should_Add_A_User_To_List_Then_Return_True()
+    {
+        // Arrange
+        AdressService adressService = new AdressService();
+        Person person = new Person
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@gmail.com",
+            PhoneNumber = "1234567890",
+            Adress = "Huvudgatan 32"
+        };
+
+        // Act
+        bool result = adressService.AddPerson(person);
+
+        // Assert
+        Assert.True(result);
+        Assert.NotNull(person);
+
+
+    }
+
+}
